03c4b70bd1c6f8435516c793ba891a71113d5c76
Kusa Mochi <whoatemyapplepie@gmail.com>
2023-11-19 15:52:36 +0900

    update sample test script.
    make sample code to take a screenshot image.

--- .test-tools/sample-test.py
+++ .test-tools/sample-test.py
@@ -1,39 +1,17 @@
 from selenium import webdriver
 import chromedriver_binary
 import time
-# import os
-# import json
+import os
 
-# BROWSER_NAME = os.environ['BROWSER_NAME']
-# HOST_NAME = os.environ['HUB_HOST']
-# BROWSER_NAME = "chrome"
+BROWSER_NAME = os.environ['BROWSER_NAME']
 
-# BROWSER_FILE_PATH = "_screenshot/" + BROWSER_NAME + "/"
-# CURRENT_DIR_PATH = os.path.dirname(os.path.abspath(__file__))
+BROWSER_FILE_PATH = "_screenshot/" + BROWSER_NAME + "/"
+CURRENT_DIR_PATH = os.path.dirname(os.path.abspath(__file__))
 
-# if not os.path.exists(os.path.join(CURRENT_DIR_PATH, BROWSER_FILE_PATH)):
-#     os.makedirs(os.path.join(CURRENT_DIR_PATH, BROWSER_FILE_PATH))
+if not os.path.exists(os.path.join(CURRENT_DIR_PATH, BROWSER_FILE_PATH)):
+    os.makedirs(os.path.join(CURRENT_DIR_PATH, BROWSER_FILE_PATH))
 
-# if BROWSER_NAME == "chrome":
-#     options = webdriver.ChromeOptions()
-# elif BROWSER_NAME == "firefox":
-#     options = webdriver.FirefoxOptions()
-# else:
-#     options = webdriver.EdgeOptions()
-#     options.use_chromium = True
-# print(BROWSER_NAME)
-
-# options.add_argument('--headless')
 # options.add_argument('--window-size=1600,900')
-
-# with webdriver.Remote(
-#     command_executor=f'http://localhost:4444/wd/hub',
-#     # desired_capabilities=options.to_capabilities(),
-#     options=options,
-# ) as driver:
-#     driver.get('https://www.ecomott.co.jp/')
-#     print(driver.current_url)
-#     driver.save_screenshot(os.path.join(CURRENT_DIR_PATH, BROWSER_FILE_PATH + "test.png"))
 
 command_executor = f'http://selenium-hub:4444/wd/hub'
 options = webdriver.ChromeOptions()
@@ -46,8 +24,8 @@ with webdriver.Remote(
 ) as driver:
     driver.maximize_window()
     time.sleep(5)
-    driver.get("https://www.ecomott.co.jp/")
+    driver.get("https://slash-mochi.net/")
     print(driver.current_url)
-    # driver.save_screenshot(os.path.join)
+    driver.save_screenshot(os.path.join(CURRENT_DIR_PATH, BROWSER_FILE_PATH, "test.png"))
     time.sleep(10)
     print('test completed!')
